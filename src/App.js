9e2b3fcc194a61c9db20a29ee15f46bd579e9293
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-10-23 11:09:20 -0700

    Fixed CORS error by adding Pokedex.js from seperate repo (look in comments) and using that to get the Pokemon evolution info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import evolutions from 'evolutions'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import evolutions from "evolutions";
+import pokedex from "./pokedex";
 
 class App extends Component {
   constructor(props) {
@@ -10,40 +11,65 @@ class App extends Component {
       searchInput: ""
     };
   }
-  handleEvolve = (e) => {
-    e.preventDefault()
-    console.log('evolve button clicked');
+  handleEvolve = e => {
+    e.preventDefault();
+    console.log("evolve button clicked");
     try {
-      if(evolutions.exists(this.state.searchInput)){
-        console.log('in the if statement');
-        console.log(evolutions.getEvolutionChain(this.state.searchInput)); //This is throwing an error.
+      if (evolutions.exists(this.state.searchInput)) {
+        console.log("in the if statement");
+        console.log(
+          Object.entries(pokedex.BattlePokedex[this.state.searchInput])
+        ); //This is throwing an error.
       }
-    }
-    catch(e){
-      console.log('The error sent back: ',e);
+    } catch (e) {
+      console.log("The error sent back: ", e);
       //Will setup a redirect here when the error is caught.
     }
-  }
+  };
   render() {
     // console.log('trying exsist on moltres',evolutions.exists('moltres'))
     return (
       <div className="App">
         <div className="App-main">
-          <h1 className="App-title">Ev<img src={logo} className="App-logo" alt="logo" />lutions</h1>
+          <h1 className="App-title">
+            Ev<img src={logo} className="App-logo" alt="logo" />lutions
+          </h1>
           <form onSubmit={this.handleEvolve}>
             <div className="App-search_bar_container">
               <div className="App-search_bar">
-                <input type="search" placeholder="Pokemon's name or ID" onChange={e => this.setState({searchInput: e.target.value})} value={this.state.searchInput}/>
+                <input
+                  type="search"
+                  placeholder="Pokemon's name or ID"
+                  onChange={e => this.setState({ searchInput: e.target.value })}
+                  value={this.state.searchInput}
+                />
               </div>
               <div className="App-search_bar_icon">
-                <svg style={{ width: 24 + 'px', height: 24 + 'px' }} viewBox="0 0 24 24">
-                  <path fill="#000000" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+                <svg
+                  style={{ width: 24 + "px", height: 24 + "px" }}
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#000000"
+                    d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
+                  />
                 </svg>
-              </div><br></br>
+              </div>
+              <br />
             </div>
-            <input type="submit" value="Evolve!" name="evolveActionButton" className="App-button"/>
+            <input
+              type="submit"
+              value="Evolve!"
+              name="evolveActionButton"
+              className="App-button"
+            />
           </form>
-            <input type="submit" value="I'm Feeling Hype" name="randomActionButton" className="App-button" />
+          <input
+            type="submit"
+            value="I'm Feeling Hype"
+            name="randomActionButton"
+            className="App-button"
+          />
         </div>
       </div>
     );
