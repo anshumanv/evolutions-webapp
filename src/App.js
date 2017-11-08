db722e4c33aa62f9e64288a8e12ba614e9052376
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-11-08 06:35:55 -0800

    Evolution search - Fix CORS issue (#13) (#14)
    * Add some of the features needed for evolution search.  Running into a bug when using .getEvolutionChain
    
    * Fixed CORS error by adding Pokedex.js from seperate repo (look in comments) and using that to get the Pokemon evolution info
    
    * Added a check for the evos property
    
    I added a check to see if the evos property exists in the searched pokemon
    
    * added public folder with index back
    
    * added a while loop to list all of pokemones evolutions if it has any
    
    * added toLowerCase method to prevent error from capitalizing pokemon names
    
    * simple refactor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import evolutions from 'evolutions';
+
+import logo from './logo.svg';
+import './App.css';
+import pokedex from './pokedex';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: '',
+    };
+    this.handleEvolve = this.handleEvolve.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleEvolve(event) {
+    event.preventDefault();
+    console.log('evolve button clicked');
+    let evo = this.state.searchInput.toLowerCase();
+    try {
+      if (evolutions.exists(evo)) {
+        if (Object.prototype.hasOwnProperty.call(pokedex.BattlePokedex[evo], 'evos')) {
+          while (Object.prototype.hasOwnProperty.call(pokedex.BattlePokedex[evo], 'evos')) {
+            evo = Object.values(pokedex.BattlePokedex[evo].evos);
+            console.log(evo);
+          }
+        } else {
+          console.log(Object.entries(pokedex.BattlePokedex[evo]));
+        }
+      }
+    } catch (error) {
+      console.trace('The error sent back: ', error);
+      // Will setup a redirect here when the error is caught.
+    }
+  }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
   render() {
+    // console.log('trying exsist on moltres',evolutions.exists('moltres'))
     return (
       <div className="App">
         <div className="App-main">
           <h1 className="App-title">
             Ev<img src={logo} className="App-logo" alt="logo" />lutions
           </h1>
-          <form>
+          <form onSubmit={this.handleEvolve}>
             <div className="App-search_bar_container">
               <div className="App-search_bar">
-                <input type="search" placeholder="Pokemon's name or ID" />
+                <input
+                  type="search"
+                  name="searchInput"
+                  placeholder="Pokemon's name or ID"
+                  onChange={this.handleChange}
+                />
               </div>
               <div className="App-search_bar_icon">
-                <svg
-                  style={{ width: 24 + "px", height: 24 + "px" }}
-                  viewBox="0 0 24 24"
-                >
+                <svg style={{ width: `${24}px`, height: `${24}px` }} viewBox="0 0 24 24">
                   <path
                     fill="#000000"
                     d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
@@ -27,19 +65,13 @@ class App extends Component {
               </div>
               <br />
             </div>
-            <input
-              type="submit"
-              value="Evolve!"
-              name="evolveActionButton"
-              className="App-button"
-            />
-            <input
-              type="submit"
-              value="I'm Feeling Hype"
-              name="randomActionButton"
-              className="App-button"
-            />
+            <button type="submit" className="App-button" name="evolveActionButton">
+              Evolve
+            </button>
           </form>
+          <button type="submit" className="App-button" name="randomActionButton">
+            I'm Feeling Hype
+          </button>
         </div>
       </div>
     );
