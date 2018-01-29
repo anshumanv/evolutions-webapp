57e0bbc51214450b24473ccab28562883e9b3393
Rohan Singh <rohaniiitv@gmail.com>
2018-01-30 01:24:07 +0530

    rendering different backgrounds

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,23 @@ import logo from '../logo.svg';
 import UserInput from './UserInput';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.run = this.run.bind(this);
+  }
+
+  run(interval, frames) {
+    let int = 1;
+
+    function func() {
+      document.body.id = `b${int}`;
+      int++;
+      if (int === frames) {
+        int = 1;
+      }
+    }
+    const swap = window.setInterval(func, interval);
+  }
   render() {
     return (
       <div className="App">
@@ -10,7 +27,7 @@ class App extends Component {
           <h1 className="App-title">
             Ev<img src={logo} className="App-logo" alt="logo" />lutions
           </h1>
-          <UserInput />
+          <UserInput backgnd={this.run(1000, 10)} />
         </div>
       </div>
     );
