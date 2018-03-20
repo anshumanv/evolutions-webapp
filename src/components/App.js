68ee5a286b30c86254ff41913ddec522529fae0f
Anshuman Verma <anshu.av97@gmail.com>
2018-03-20 09:19:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,24 +6,23 @@ import '../css/index.css';
 class App extends Component {
 	
 	componentDidMount(){
-		var imageArray = [
-            'http://hdfreewallpaper.net/wp-content/uploads/2016/08/Pokemon-hd-Wallpaper-sumsung-download.jpg',
-            'https://wallpapercave.com/wp/Qf9SNmS.png',
-            'https://images3.alphacoders.com/273/273289.jpg',
-            'https://images4.alphacoders.com/574/thumb-1920-574726.jpg'
+		let imageArray = [
+			'https://images3.alphacoders.com/273/273289.jpg',
+			'https://images4.alphacoders.com/574/thumb-1920-574726.jpg',
+			'https://img00.deviantart.net/0093/i/2005/221/1/7/wobbuffet_wallpaper_by_xebeckle_il_ziluf.jpg',
+			'http://3.bp.blogspot.com/-PzgwkqKiT0c/VTvAK_eDQrI/AAAAAAAALuk/KkthF3qFt9A/s1600/pokemon-tumblr-wallpaper-6805925.jpg',
+			'http://www.qygjxz.com/data/out/252/4240257-wallpapers-pokemon.png'
 		],
-			currentIndex = 0,
-			Duration = 2000;
+		currentIndex = 0,
+		Duration = 10000;
 
-			function refreshingBackground()
-			{
-				document.body.style.backgroundImage = `url(${imageArray[currentIndex]})`
-				currentIndex++;
-				if(currentIndex===imageArray.length){currentIndex=0;}
-				setTimeout(refreshingBackground,Duration);
-			}
-
-			refreshingBackground();
+		function refreshingBackground() {
+			document.body.style.backgroundImage = `url(${imageArray[currentIndex]})`
+			currentIndex++;
+			if(currentIndex === imageArray.length){currentIndex=0;}
+			setTimeout(refreshingBackground, Duration);
+		}
+		refreshingBackground();
 
 	}
 	
