e6d398d25024481b9bfb383aced0623541f7c08a
jmeline <jacob.meline@gmail.com>
2018-10-03 00:24:07 -0600

    Fixed styling around basic stats section of hover. exported styles into variables

--- src/components/PokemonSprite.js
+++ src/components/PokemonSprite.js
@@ -4,22 +4,65 @@ import PokeSprite from 'react-poke-sprites';
 import 'react-tippy/dist/tippy.css';
 import { Tooltip } from 'react-tippy';
 
-const Info = ({ info }) => {
-  return (
-    <React.Fragment>
-      <div>name: {info.species} </div>
-      <div>color: {info.color} </div>
-      <div>weight: {info.weightkg} </div>
-      <div>baseStats: {JSON.stringify(info.baseStats)} </div>
-    </React.Fragment>
-  );
+const containerStyle = {
+  textAlign: 'left',
+  width: 300,
 };
 
+const colStyle = {
+  float: 'left',
+  width: '33%',
+};
+
+const rowStyle = {
+  overflow: 'auto',
+  height: '100%',
+};
+
+const Stats = ({ baseStats }) => (
+  <React.Fragment>
+    <div> Stats: </div>
+    <div style={rowStyle}>
+      <div style={colStyle}>Hp: {baseStats.hp} </div>
+      <div style={colStyle}>Atk: {baseStats.atk} </div>
+      <div style={colStyle}>Def: {baseStats.def} </div>
+    </div>
+    <div style={rowStyle}>
+      <div style={colStyle}> Sp.Atk: {baseStats.spa} </div>
+      <div style={colStyle}> Sp.Def: {baseStats.def} </div>
+      <div style={colStyle}> Sp: {baseStats.spe} </div>
+    </div>
+  </React.Fragment>
+);
+
+const Info = ({ info }) => (
+  <div style={containerStyle}>
+    <div>
+      [{info.num}] {info.species} ({info.types.join(', ')})
+    </div>
+    <div>
+      Weight: {info.weightkg} kg, Height: {info.heightm} m
+    </div>
+    <div> Abilities: {Object.values(info.abilities).join(', ')} </div>
+    <Stats baseStats={info.baseStats} />
+  </div>
+);
+
 const PokemonSprite = ({ pokemon, info }) => (
-  <Tooltip html={<Info info={info} />} position="bottom">
+  <Tooltip
+    arrow
+    interactive
+    position="bottom"
+    arrowSize="big"
+    html={<Info info={info} />}
+  >
     <PokeSprite pokemon={pokemon} className="pokemons" alt={pokemon} />
   </Tooltip>
 );
+
+Stats.propTypes = {
+  baseStats: PropTypes.object,
+};
 
 Info.propTypes = {
   info: PropTypes.object,
